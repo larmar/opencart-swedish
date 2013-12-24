12553a89ade322b0a018e254aa89f86df7d6b35d
Martin Wilderoth <martin.wilderoth@linserv.se>
2013-12-24 20:35:27 +0100

    New file account

---
+++ catalog/language/swedish/account/recurring.php
@@ -0,0 +1,55 @@
+<?php
+$_['heading_title'] = 'Återkommande betalningar';
+$_['button_continue'] = 'Fortsätt';
+$_['button_view'] = 'Visa';
+$_['button_cancel_profile'] = 'Annulera betalningar';
+$_['text_empty'] = 'Inga återkommande betalningsprofiler finns';
+$_['text_product'] = 'Produkt:';
+$_['text_order'] = 'Order:';
+$_['text_quantity'] = 'Antal:';
+$_['text_account'] = 'Konto';
+$_['text_action'] = 'Åtgärd';
+$_['text_recurring'] = 'Återkommande betalning';
+$_['text_transactions'] = 'Transaktioner';
+$_['button_return'] = 'Avkastning';
+$_['text_confirm_cancel'] = 'Är du säker på att du vill avbryta profilen ?';
+$_['text_empty_transactions'] = 'Inga transaktioner för den här profilen';
+
+$_['column_created'] = 'Skapad';
+$_['column_type'] = 'Typ';
+$_['column_amount'] = 'Mängd';
+$_['column_status'] = 'Status';
+$_['column_product'] = 'Produkt';
+$_['column_action'] = 'Åtgärd';
+$_['column_profile_id'] = 'Profil ID';
+
+$_['text_recurring_detail'] = 'Återkommande betalningsinformation';
+$_['text_recurring_id'] = 'Profil ID:';
+$_['text_payment_method'] = 'Betalningssätt:';
+$_['text_date_added'] = 'Skapad:';
+$_['text_recurring_description'] = 'Beskrivning:';
+$_['text_status'] = 'Status:';
+$_['text_ref'] = 'Referens:';
+
+$_['text_status_active'] = 'Aktiv';
+$_['text_status_inactive'] = 'Inaktiv';
+$_['text_status_cancelled'] = 'Annulerad';
+$_['text_status_suspended'] = 'Avstängd';
+$_['text_status_expired'] = 'Utgånget';
+$_['text_status_pending'] = 'I väntan';
+
+$_['text_transaction_created'] = 'Skapad';
+$_['text_transaction_payment'] = 'Betalning';
+$_['text_transaction_outstanding_payment'] = 'Utomordentligt betalning';
+$_['text_transaction_skipped'] = 'Betalning hoppade';
+$_['text_transaction_failed'] = 'Betalning misslyckades';
+$_['text_transaction_cancelled'] = 'Annulerad';
+$_['text_transaction_suspended'] = 'Avstängd';
+$_['text_transaction_suspended_failed'] = 'Avstängd från misslyckades betalning';
+$_['text_transaction_outstanding_failed'] = 'Utestående betalning misslyckades';
+$_['text_transaction_expired'] = 'Utgånget';
+
+$_['error_not_cancelled'] = 'Fel: %s';
+$_['error_not_found'] = 'Det gick inte att avbryta profil';
+$_['success_cancelled'] = 'Återkommande betalning har avbrutits';
+?>
