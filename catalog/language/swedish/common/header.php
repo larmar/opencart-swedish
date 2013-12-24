92349692378dd13a47f0d9e87c789d7d1f285d78
Martin Wilderoth <martin.wilderoth@linserv.se>
2013-12-24 22:13:02 +0100

    Layout fixes

--- catalog/language/swedish/common/header.php
+++ catalog/language/swedish/common/header.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_home']     = 'Hem';
-$_['text_wishlist'] = 'Önskelista (%s)';
-$_['text_shopping_cart']     = 'Varukorg';
-$_['text_search']   = 'Sök';
-$_['text_welcome']  = 'Välkommen! Du kan <a href="%s">logga in</a> eller <a href="%s">skapa konto</a>.';
-$_['text_logged']   = 'Du är inloggad som <a href="%s">%s</a> <b>(</b> <a href="%s">Logga ut</a> <b>)</b>';
-$_['text_account']  = 'Mitt konto';
+$_['text_home']           = 'Hem';
+$_['text_wishlist']       = 'Önskelista (%s)';
+$_['text_shopping_cart']  = 'Varukorg';
+$_['text_search']         = 'Sök';
+$_['text_welcome']        = 'Välkommen! Du kan <a href="%s">logga in</a> eller <a href="%s">skapa konto</a>.';
+$_['text_logged']         = 'Du är inloggad som <a href="%s">%s</a> <b>(</b> <a href="%s">Logga ut</a> <b>)</b>';
+$_['text_account']        = 'Mitt konto';
 $_['text_checkout']       = 'Kassa';
 ?>
