01b99684568409cbb788f9bfc13d941a65e765d6
Martin Wilderoth <martin.wilderoth@linserv.se>
2013-12-24 22:29:55 +0100

    ebay files

---
+++ catalog/language/swedish/ebay/order.php
@@ -0,0 +1,6 @@
+<?php
+$_['lang_shipping']     = 'Leverans';
+$_['lang_discount']     = 'Rabatt';
+$_['lang_tax']          = 'skatt';
+$_['lang_subtotal']     = 'Delsumma';
+$_['lang_total']        = 'Totalt';
---
+++ catalog/language/swedish/ebay/stock_report.php
@@ -0,0 +1,3 @@
+<?php
+$_['title']         = 'OpenBay Pro lager länk rapport';
+$_['help']          = 'Klicka här för support';
