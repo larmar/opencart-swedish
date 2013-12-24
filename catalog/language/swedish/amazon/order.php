14633b29236b62765078a4da5badb0cad34164fa
Martin Wilderoth <martin.wilderoth@linserv.se>
2013-12-24 20:52:37 +0100

    Amazon + US

---
+++ catalog/language/swedish/amazon/order.php
@@ -0,0 +1,10 @@
+<?php
+
+$_['paid_on_amazon_text'] = 'Betalas på Amazon';
+$_['shipping_text'] = 'Leverans';
+$_['shipping_tax_text'] = 'Skatt på frakt';
+$_['gift_wrap_text'] = 'Presentinslagning';
+$_['gift_wrap_tax_text'] = 'Skatt på Presentinslagning';
+$_['sub_total_text'] = 'Delsumma';
+$_['tax_text'] = 'Skatt';
+$_['total_text'] = 'Totaltsumma';
---
+++ catalog/language/swedish/amazonus/order.php
@@ -0,0 +1,9 @@
+<?php
+$_['paid_on_amazonus_text'] = 'Betalas på Amazon USA';
+$_['shipping_text'] = 'Leverans';
+$_['shipping_tax_text'] = 'Skatt på Frakt';
+$_['gift_wrap_text'] = 'Presentinslagning';
+$_['gift_wrap_tax_text'] = 'Skatt på Presentinslagning';
+$_['sub_total_text'] = 'Delsumma';
+$_['tax_text'] = 'Skatt';
+$_['total_text'] = 'Totalsumma';
