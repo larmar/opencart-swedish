e37640d9c3390785ff58a2991505f9d47588883a
Martin Wilderoth <martin.wilderoth@linserv.se>
2013-12-29 20:59:17 +0100

    Changed from Nerladdning to Nedladdning

--- catalog/language/swedish/account/download.php
+++ catalog/language/swedish/account/download.php
@@ -1,14 +1,14 @@
 <?php
 // Heading 
-$_['heading_title']   = 'Konto nerladdningar';
+$_['heading_title']   = 'Konto nedladdningar';
 
 // Text
 $_['text_account']    = 'Konto';
-$_['text_downloads']  = 'Nerladdningar';
+$_['text_downloads']  = 'Nedladdningar';
 $_['text_order']      = 'Ordernummer:';
 $_['text_date_added'] = 'Datum:';
 $_['text_name']       = 'Namn:';
 $_['text_remaining']  = 'Kvarståde:';
 $_['text_size']       = 'Storlek:';
-$_['text_empty']      = 'Du har inte gjort några tidigare ordrar med nerladdningsbar material!';
+$_['text_empty']      = 'Du har inte gjort några tidigare ordrar med nedladdningsbart material!';
 ?>
--- catalog/language/swedish/mail/order.php
+++ catalog/language/swedish/mail/order.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_new_subject']          = '%s - Order %s';
-$_['text_new_greeting']         = 'Thank you for your interest in %s products. Your order has been received and will be processed once payment has been confirmed.';
+$_['text_new_greeting']         = 'Tack för att du beställer produkter hos på %s. Vi kommer att hantera din order vidare.';
 $_['text_new_received']         = 'Du har mottagit en order.';
 $_['text_new_link']             = 'för att se din order klicka på länken nedan:';
 $_['text_new_order_detail']     = 'Orderdetaljer';
@@ -23,12 +23,12 @@ $_['text_new_quantity']         = 'Antal';
 $_['text_new_price']            = 'Pris';
 $_['text_new_order_total']      = 'Order Total';	
 $_['text_new_total']            = 'TOTAL (Att betala)';	
-$_['text_new_download']         = 'När din betalning är bekräftad så kan du klicka på länken nedan för åtkomst för dina nerladdningsbara produkter:';
+$_['text_new_download']         = 'När din betalning är bekräftad så kan du klicka på länken nedan för åtkomst för dina nedladdningsbara produkter:';
 $_['text_new_comment']          = 'Din kommentar till din order är:';
 $_['text_new_footer']           = 'Om du har några frågor kan du svara på detta mail.';
 $_['text_new_powered']          = 'Powered By <a href="http://www.opencart.com">OpenCart</a>.';
 $_['text_update_subject']       = '%s - Order Uppdatering %s';
-$_['text_update_order']         = 'Order ID:';
+$_['text_update_order']         = 'Orderid:';
 $_['text_update_date_added']    = 'Datum:';
 $_['text_update_order_status']  = 'Din order har uppdaterats med följande status:';
 $_['text_update_comment']       = 'Kommentaren till din order är:';
